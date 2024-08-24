254a9958569dcdf12e11c1f30a386377a7289498
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-08-24 01:40:30 -0400

    feat: add cli

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![stats_provider](assets/stats_provider.png)
 
-This is a simple event provider for [Sketchybar](https://github.com/FelixKratz/SketchyBar?tab=readme-ov-file) that sends the current CPU, memory and disk usage percentages to Sketchybar every 5 seconds as the event `system_stats`.
+This is a simple event provider for [Sketchybar](https://github.com/FelixKratz/SketchyBar?tab=readme-ov-file) that sends system stats to Sketchybar as the event trigger `system_stats`.
 
 ## Build instructions
 
@@ -12,34 +12,107 @@ At this time it is necessary to compile and install the crate locally. The simpl
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 ```
 
-Then use cargo to build the binary:
+Then use `cargo` to build the binary:
 
 ```bash
 cargo build --release
 ```
 
+## CLI usage
+
+Use the `help` command to get usage information:
+```console
+$ stats_provider --help
+A simple system stats event provider for Sketchybar.
+
+Usage: stats_provider [OPTIONS]
+
+Options:
+  -a, --all                   Get all stats
+  -c, --cpu [<CPU>...]        [possible values: count, usage]
+  -d, --disk [<DISK>...]      [possible values: count, free, total, usage, used]
+  -m, --memory [<MEMORY>...]  [possible values: free, total, usage, used]
+  -i, --interval <INTERVAL>   Refresh interval in seconds [default: 5]
+      --verbose               Enable verbose output
+  -h, --help                  Print help
+  -V, --version               Print version
+```
+
+For example, to get usage percentages at a refresh interval of 2 seconds:
+```bash
+stats_provider --cpu usage --disk usage --memory usage --interval 2
+```
+
+Add the `--verbose` flag to see more detailed output:
+
+```console
+$ stats_provider --cpu usage --disk usage --memory usage --interval 2 --verbose
+SketchyBar Stats Provider is running.
+Stats Provider CLI: Cli { all: false, cpu: Some(["usage"]), disk: Some(["usage"]), memory: Some(["usage"]), interval: 2, verbose: true }
+Current message: CPU_USAGE="4%" MEMORY_USAGE="59%" DISK_USAGE="64%"
+Successfully sent to SketchyBar: --trigger system_stats CPU_USAGE="4%" MEMORY_USAGE="59%" DISK_USAGE="64%"
+Current message: CPU_USAGE="3%" MEMORY_USAGE="59%" DISK_USAGE="64%"
+Successfully sent to SketchyBar: --trigger system_stats CPU_USAGE="3%" MEMORY_USAGE="59%" DISK_USAGE="64%"
+Current message: CPU_USAGE="2%" MEMORY_USAGE="59%" DISK_USAGE="64%"
+Successfully sent to SketchyBar: --trigger system_stats CPU_USAGE="2%" MEMORY_USAGE="59%" DISK_USAGE="64%"
+Current message: CPU_USAGE="7%" MEMORY_USAGE="59%" DISK_USAGE="64%"
+Successfully sent to SketchyBar: --trigger system_stats CPU_USAGE="7%" MEMORY_USAGE="59%" DISK_USAGE="64%"
+```
+
+## Usage with Sketchybar
+
+[!TODO] Test this example with a `sketchybarrc` config.
+
+Run the `stats_provider` event provider with the desired options. Alternatively, you can add it to your `sketchybarrc` config:
+
+```bash
+killall stats_provider
+# Update with path to stats_provider
+$CONFIG_DIR/stats_provider/target/release/stats_provider --cpu usage --disk usage --memory usage &
+```
+
+Subscribe to the `system_stats` event and update use the environment variables to update items.
+
+The following example shows how to use the `stats_provider` event provider to update the disk usage percentage.
+
+```bash
+sketchybar --add item disk right         \
+           --set disk                    \
+                 label=$DISK_USAGE       \
+           --subscribe disk system_stats
+```
+
+### Environment variables that can be provided by the `system_stats` event
+
+| Variable       | Description         |
+| -------------- | ------------------- |
+| `CPU_COUNT`    | Number of CPU cores |
+| `CPU_USAGE`    | CPU usage %         |
+| `DISK_COUNT`   | Number of disks     |
+| `DISK_FREE`    | Free disk space GB  |
+| `DISK_TOTAL`   | Total disk space GB |
+| `DISK_USAGE`   | Disk usage %        |
+| `DISK_USED`    | Used disk space GB  |
+| `MEMORY_FREE`  | Free memory GB      |
+| `MEMORY_TOTAL` | Total memory GB     |
+| `MEMORY_USAGE` | Memory usage %      |
+| `MEMORY_USED`  | Used memory GB      |
+
 ## Sample usage with SbarLua
 
-The following example shows how to use the `stats_provider` event provider to get the disk usage percentage.
+The following example shows how to use the `stats_provider` event provider to update the disk usage percentage.
 ```lua
 -- Update with path to stats_provider
-sbar.exec('killall stats_provider >/dev/null; $CONFIG_DIR/stats_provider/target/release/stats_provider')
+sbar.exec('killall stats_provider >/dev/null; $CONFIG_DIR/stats_provider/target/release/stats_provider --cpu usage --disk usage --memory usage')
 
 -- Subscribe and use the `DISK_USAGE` var
 local disk = sbar.add('item', 'disk', {
 	position = 'right',
-	icon = {
-		font = 'A nerd font'
-		string = '',
-		padding_right = 0
-	},
 })
 disk:subscribe('system_stats', function(env)
 	disk:set { label = env.DISK_USAGE }
 end)
 ```
-
-CPU and memory usage can be obtained in the same way by using the `CPU_USAGE` and `MEMORY_USAGE` vars.
 
 ## Thanks
 
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,192 @@
 extern crate sketchybar_rs;
 
+use clap::Parser;
 use std::thread;
 use std::time::Duration;
 use sysinfo::{CpuRefreshKind, Disks, MemoryRefreshKind, RefreshKind, System};
 use tokio::task;
 
-fn cpu_usage(s: &mut System) -> f32 {
-    s.refresh_cpu_all();
-    let total_usage: f32 = s.cpus().iter().map(|cpu| cpu.cpu_usage()).sum();
+#[derive(Parser, Debug)]
+#[command(name = "stats_provider", version, about, long_about = None, arg_required_else_help = true)]
+struct Cli {
+    #[arg(short = 'a', long, help = "Get all stats")]
+    all: bool,
+
+    #[arg(short = 'c', long, num_args = 0.., value_parser = ["count", "usage"])]
+    cpu: Option<Vec<String>>,
+
+    #[arg(short = 'd', long, num_args = 0.., value_parser = ["count", "free", "total", "usage", "used"])]
+    disk: Option<Vec<String>>,
+
+    #[arg(short = 'm', long, num_args = 0.., value_parser = ["free", "total", "usage", "used"])]
+    memory: Option<Vec<String>>,
+
+    #[arg(
+        short = 'i',
+        long,
+        default_value_t = 5,
+        help = "Refresh interval in seconds"
+    )]
+    interval: u32,
+
+    #[arg(long, default_value_t = false, help = "Enable verbose output")]
+    verbose: bool,
+}
+
+pub struct MessageBuilder {
+    parts: Vec<String>,
+}
+
+impl MessageBuilder {
+    pub fn new() -> Self {
+        Self { parts: Vec::new() }
+    }
+
+    pub fn add_part(mut self, part: String) -> Self {
+        self.parts.push(part);
+        self
+    }
+
+    pub fn build(self) -> String {
+        self.parts.join(" ")
+    }
+}
+
+const BYTES_PER_GB: f32 = 1_073_741_824.0;
+
+fn get_cpu_stats(s: &System, flags: &[String]) -> String {
+    fn cpu_usage(cpu_count: f32, total_usage: f32) -> String {
+        let avg_cpu_usage: f32 = (total_usage / cpu_count).round();
+        format!("CPU_USAGE=\"{:.0}%\"", avg_cpu_usage)
+    }
+
+    let mut result = Vec::new();
+
     let cpu_count = s.cpus().len() as f32;
-    total_usage / cpu_count
+    let total_usage: f32 = s.cpus().iter().map(|cpu| cpu.cpu_usage()).sum();
+
+    for flag in flags {
+        match flag.as_str() {
+            "count" => {
+                result.push(format!("CPU_COUNT=\"{}\"", cpu_count));
+            }
+            "usage" => {
+                result.push(cpu_usage(cpu_count, total_usage));
+            }
+            _ => {
+                result.push(format!("CPU_{}=\"\"", flag));
+            }
+        }
+    }
+
+    result.join(" ")
 }
 
-fn disk_space() -> (u64, u64) {
+fn get_disk_stats(flags: &[String]) -> String {
+    let mut result = Vec::new();
+
     let disks = Disks::new_with_refreshed_list();
-    let (total_space, available_space) =
-        disks
-            .list()
-            .iter()
-            .fold((0, 0), |(total, available), disk| {
-                (
-                    total + disk.total_space(),
-                    available + disk.available_space(),
-                )
-            });
-    (total_space, available_space)
+    let (total_space, used_space) = disks.list().iter().fold((0, 0), |(total, used), disk| {
+        (
+            total + disk.total_space(),
+            used + disk.total_space() - disk.available_space(),
+        )
+    });
+    let disk_usage_percentage = if total_space > 0 {
+        ((used_space as f32 / total_space as f32) * 100.0).round() as u32
+    } else {
+        0
+    };
+
+    for flag in flags {
+        match flag.as_str() {
+            "count" => {
+                result.push(format!("DISK_COUNT=\"{}\"", disks.list().len()));
+            }
+            "free" => {
+                result.push(format!(
+                    "DISK_FREE=\"{:.1}GB\"",
+                    (total_space as f32 - used_space as f32) / BYTES_PER_GB
+                ));
+            }
+            "total" => {
+                result.push(format!(
+                    "DISK_TOTAL=\"{:.1}GB\"",
+                    total_space as f32 / BYTES_PER_GB
+                ));
+            }
+            "usage" => {
+                result.push(format!("DISK_USAGE=\"{:.0}%\"", disk_usage_percentage));
+            }
+            "used" => {
+                result.push(format!(
+                    "DISK_USED=\"{:.1}GB\"",
+                    used_space as f32 / BYTES_PER_GB
+                ));
+            }
+            _ => {}
+        }
+    }
+
+    result.join(" ")
 }
 
-fn memory_usage(s: &System) -> (u64, u64) {
-    (s.total_memory(), s.used_memory())
+fn get_memory_stats(s: &System, flags: &[String]) -> String {
+    let mut result = Vec::new();
+
+    let total_memory = s.total_memory();
+    let used_memory = s.used_memory();
+    let memory_usage_percentage = ((used_memory as f32 / total_memory as f32) * 100.0).round();
+
+    for flag in flags {
+        match flag.as_str() {
+            "free" => {
+                result.push(format!(
+                    "MEMORY_FREE=\"{:.1}GB\"",
+                    (total_memory as f32 - used_memory as f32) / BYTES_PER_GB
+                ));
+            }
+            "total" => {
+                result.push(format!(
+                    "MEMORY_TOTAL=\"{:.1}GB\"",
+                    total_memory as f32 / BYTES_PER_GB
+                ));
+            }
+            "usage" => {
+                result.push(format!("MEMORY_USAGE=\"{:.0}%\"", memory_usage_percentage));
+            }
+            "used" => {
+                result.push(format!(
+                    "MEMORY_USED=\"{:.1}GB\"",
+                    used_memory as f32 / BYTES_PER_GB
+                ));
+            }
+            _ => {}
+        }
+    }
+
+    result.join(" ")
 }
 
-async fn send_to_sketchybar(event: &str, vars: String) {
+async fn send_to_sketchybar(event: &str, vars: String, verbose: bool) {
     let command = format!("--trigger {} {}", event, vars);
 
     if let Err(e) = sketchybar_rs::message(&command, None) {
         eprintln!("Failed to send to SketchyBar: {}", e);
-    } else {
+    } else if verbose {
         println!("Successfully sent to SketchyBar: {}", command);
     }
 }
 
 #[tokio::main]
 async fn main() {
+    let cli = Cli::parse();
+
+    if cli.verbose {
+        println!("SketchyBar Stats Provider is running.");
+        println!("Stats Provider CLI: {:?}", cli);
+    }
+
     let mut s = System::new_with_specifics(
         RefreshKind::new()
             .with_cpu(CpuRefreshKind::new().with_cpu_usage())
@@ -50,25 +194,58 @@ async fn main() {
     );
 
     loop {
-        let cpu_avg_usage = cpu_usage(&mut s);
-        let (disk_total, disk_available) = disk_space();
-        let (memory_total, memory_used) = memory_usage(&s);
-
-        let disk_used = disk_total - disk_available;
-        let disk_usage_percentage = ((disk_used as f32 / disk_total as f32) * 100.0).round() as u32;
-        let memory_usage_percentage =
-            ((memory_used as f32 / memory_total as f32) * 100.0).round() as u32;
-
-        let vars = format!(
-            "CPU_USAGE=\"{:.0}%\" MEMORY_USAGE=\"{:.0}%\" DISK_USAGE=\"{:.0}%\"",
-            cpu_avg_usage.round() as u32,
-            memory_usage_percentage,
-            disk_usage_percentage
+        s.refresh_specifics(
+            RefreshKind::new()
+                .with_cpu(CpuRefreshKind::new().with_cpu_usage())
+                .with_memory(MemoryRefreshKind::new().with_ram()),
         );
+        let mut builder = MessageBuilder::new();
 
-        println!("Program is running. Current message: {}", vars);
-        task::spawn(async move { send_to_sketchybar("system_stats", vars).await });
+        let mut commands = Vec::new();
 
-        thread::sleep(Duration::from_secs(5));
+        let all_cpu_flags = vec!["count".to_string(), "usage".to_string()];
+        let all_memory_flags = vec![
+            "free".to_string(),
+            "total".to_string(),
+            "usage".to_string(),
+            "used".to_string(),
+        ];
+        let all_disk_flags = vec![
+            "count".to_string(),
+            "free".to_string(),
+            "total".to_string(),
+            "usage".to_string(),
+            "used".to_string(),
+        ];
+
+        if cli.all {
+            commands.push(get_cpu_stats(&s, &all_cpu_flags));
+            commands.push(get_memory_stats(&s, &all_memory_flags));
+            commands.push(get_disk_stats(&all_disk_flags));
+        } else {
+            if let Some(cpu_flags) = &cli.cpu {
+                commands.push(get_cpu_stats(&s, cpu_flags));
+            }
+
+            if let Some(memory_flags) = &cli.memory {
+                commands.push(get_memory_stats(&s, memory_flags));
+            }
+
+            if let Some(disk_flags) = &cli.disk {
+                commands.push(get_disk_stats(disk_flags));
+            }
+        }
+
+        for cmd in &commands {
+            builder = builder.add_part(cmd.to_string());
+        }
+        let message = builder.build();
+
+        if cli.verbose {
+            println!("Current message: {}", message);
+        }
+        task::spawn(async move { send_to_sketchybar("system_stats", message, cli.verbose).await });
+
+        thread::sleep(Duration::from_secs(cli.interval.into()));
     }
 }
