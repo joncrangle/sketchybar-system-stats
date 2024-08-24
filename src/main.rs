0faa6971f49d901d0ee35a11c6fb5a06e94a330e
jonathancrangle <94425204+joncrangle@users.noreply.github.com>
2024-08-24 12:00:45 -0400

    chore: refactor and remove async

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use clap::Parser;
 use std::thread;
 use std::time::Duration;
 use sysinfo::{CpuRefreshKind, Disks, MemoryRefreshKind, RefreshKind, System};
-use tokio::task;
 
 #[derive(Parser, Debug)]
 #[command(name = "stats_provider", version, about, long_about = None, arg_required_else_help = true)]
@@ -33,58 +32,31 @@ struct Cli {
     verbose: bool,
 }
 
-pub struct MessageBuilder {
-    parts: Vec<String>,
-}
-
-impl MessageBuilder {
-    pub fn new() -> Self {
-        Self { parts: Vec::new() }
-    }
-
-    pub fn add_part(mut self, part: String) -> Self {
-        self.parts.push(part);
-        self
-    }
-
-    pub fn build(self) -> String {
-        self.parts.join(" ")
-    }
-}
-
 const BYTES_PER_GB: f32 = 1_073_741_824.0;
 
-fn get_cpu_stats(s: &System, flags: &[String]) -> String {
-    fn cpu_usage(cpu_count: f32, total_usage: f32) -> String {
-        let avg_cpu_usage: f32 = (total_usage / cpu_count).round();
-        format!("CPU_USAGE=\"{:.0}%\"", avg_cpu_usage)
-    }
-
-    let mut result = Vec::new();
-
+fn get_cpu_stats(s: &System, flags: &[&str]) -> String {
     let cpu_count = s.cpus().len() as f32;
     let total_usage: f32 = s.cpus().iter().map(|cpu| cpu.cpu_usage()).sum();
 
-    for flag in flags {
-        match flag.as_str() {
+    let mut result = String::with_capacity(100);
+
+    for &flag in flags {
+        match flag {
             "count" => {
-                result.push(format!("CPU_COUNT=\"{}\"", cpu_count));
+                result.push_str(&format!("CPU_COUNT=\"{}\" ", cpu_count));
             }
             "usage" => {
-                result.push(cpu_usage(cpu_count, total_usage));
+                let avg_cpu_usage: f32 = (total_usage / cpu_count).round();
+                result.push_str(&format!("CPU_USAGE=\"{:.0}%\" ", avg_cpu_usage));
             }
-            _ => {
-                result.push(format!("CPU_{}=\"\"", flag));
-            }
+            _ => {}
         }
     }
 
-    result.join(" ")
+    result.trim_end().to_string()
 }
 
-fn get_disk_stats(flags: &[String]) -> String {
-    let mut result = Vec::new();
-
+fn get_disk_stats(flags: &[&str]) -> String {
     let disks = Disks::new_with_refreshed_list();
     let (total_space, used_space) = disks.list().iter().fold((0, 0), |(total, used), disk| {
         (
@@ -98,29 +70,31 @@ fn get_disk_stats(flags: &[String]) -> String {
         0
     };
 
-    for flag in flags {
-        match flag.as_str() {
+    let mut result = String::with_capacity(100);
+
+    for &flag in flags {
+        match flag {
             "count" => {
-                result.push(format!("DISK_COUNT=\"{}\"", disks.list().len()));
+                result.push_str(&format!("DISK_COUNT=\"{}\" ", disks.list().len()));
             }
             "free" => {
-                result.push(format!(
-                    "DISK_FREE=\"{:.1}GB\"",
+                result.push_str(&format!(
+                    "DISK_FREE=\"{:.1}GB\" ",
                     (total_space as f32 - used_space as f32) / BYTES_PER_GB
                 ));
             }
             "total" => {
-                result.push(format!(
-                    "DISK_TOTAL=\"{:.1}GB\"",
+                result.push_str(&format!(
+                    "DISK_TOTAL=\"{:.1}GB\" ",
                     total_space as f32 / BYTES_PER_GB
                 ));
             }
             "usage" => {
-                result.push(format!("DISK_USAGE=\"{:.0}%\"", disk_usage_percentage));
+                result.push_str(&format!("DISK_USAGE=\"{:.0}%\" ", disk_usage_percentage));
             }
             "used" => {
-                result.push(format!(
-                    "DISK_USED=\"{:.1}GB\"",
+                result.push_str(&format!(
+                    "DISK_USED=\"{:.1}GB\" ",
                     used_space as f32 / BYTES_PER_GB
                 ));
             }
@@ -128,36 +102,39 @@ fn get_disk_stats(flags: &[String]) -> String {
         }
     }
 
-    result.join(" ")
+    result.trim_end().to_string()
 }
 
-fn get_memory_stats(s: &System, flags: &[String]) -> String {
-    let mut result = Vec::new();
-
+fn get_memory_stats(s: &System, flags: &[&str]) -> String {
     let total_memory = s.total_memory();
     let used_memory = s.used_memory();
     let memory_usage_percentage = ((used_memory as f32 / total_memory as f32) * 100.0).round();
 
-    for flag in flags {
-        match flag.as_str() {
+    let mut result = String::with_capacity(100);
+
+    for &flag in flags {
+        match flag {
             "free" => {
-                result.push(format!(
-                    "MEMORY_FREE=\"{:.1}GB\"",
+                result.push_str(&format!(
+                    "MEMORY_FREE=\"{:.1}GB\" ",
                     (total_memory as f32 - used_memory as f32) / BYTES_PER_GB
                 ));
             }
             "total" => {
-                result.push(format!(
-                    "MEMORY_TOTAL=\"{:.1}GB\"",
+                result.push_str(&format!(
+                    "MEMORY_TOTAL=\"{:.1}GB\" ",
                     total_memory as f32 / BYTES_PER_GB
                 ));
             }
             "usage" => {
-                result.push(format!("MEMORY_USAGE=\"{:.0}%\"", memory_usage_percentage));
+                result.push_str(&format!(
+                    "MEMORY_USAGE=\"{:.0}%\" ",
+                    memory_usage_percentage
+                ));
             }
             "used" => {
-                result.push(format!(
-                    "MEMORY_USED=\"{:.1}GB\"",
+                result.push_str(&format!(
+                    "MEMORY_USED=\"{:.1}GB\" ",
                     used_memory as f32 / BYTES_PER_GB
                 ));
             }
@@ -165,10 +142,10 @@ fn get_memory_stats(s: &System, flags: &[String]) -> String {
         }
     }
 
-    result.join(" ")
+    result.trim_end().to_string()
 }
 
-async fn send_to_sketchybar(event: &str, vars: String, verbose: bool) {
+fn send_to_sketchybar(event: &str, vars: String, verbose: bool) {
     let command = format!("--trigger {} {}", event, vars);
 
     if let Err(e) = sketchybar_rs::message(&command, None) {
@@ -178,8 +155,7 @@ async fn send_to_sketchybar(event: &str, vars: String, verbose: bool) {
     }
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let cli = Cli::parse();
 
     if cli.verbose {
@@ -187,64 +163,61 @@ async fn main() {
         println!("Stats Provider CLI: {:?}", cli);
     }
 
-    let mut s = System::new_with_specifics(
-        RefreshKind::new()
-            .with_cpu(CpuRefreshKind::new().with_cpu_usage())
-            .with_memory(MemoryRefreshKind::new().with_ram()),
-    );
+    let refresh_kind = RefreshKind::new()
+        .with_cpu(CpuRefreshKind::new().with_cpu_usage())
+        .with_memory(MemoryRefreshKind::new().with_ram());
+
+    let mut s = System::new_with_specifics(refresh_kind.clone());
+
+    let all_cpu_flags = &["count", "usage"];
+    let all_memory_flags = &["free", "total", "usage", "used"];
+    let all_disk_flags = &["count", "free", "total", "usage", "used"];
 
     loop {
-        s.refresh_specifics(
-            RefreshKind::new()
-                .with_cpu(CpuRefreshKind::new().with_cpu_usage())
-                .with_memory(MemoryRefreshKind::new().with_ram()),
-        );
-        let mut builder = MessageBuilder::new();
+        s.refresh_specifics(refresh_kind.clone());
 
-        let mut commands = Vec::new();
-
-        let all_cpu_flags = vec!["count".to_string(), "usage".to_string()];
-        let all_memory_flags = vec![
-            "free".to_string(),
-            "total".to_string(),
-            "usage".to_string(),
-            "used".to_string(),
-        ];
-        let all_disk_flags = vec![
-            "count".to_string(),
-            "free".to_string(),
-            "total".to_string(),
-            "usage".to_string(),
-            "used".to_string(),
-        ];
+        let mut commands = String::with_capacity(300);
 
         if cli.all {
-            commands.push(get_cpu_stats(&s, &all_cpu_flags));
-            commands.push(get_memory_stats(&s, &all_memory_flags));
-            commands.push(get_disk_stats(&all_disk_flags));
+            commands.push_str(&get_cpu_stats(&s, all_cpu_flags));
+            commands.push_str(" ");
+            commands.push_str(&get_memory_stats(&s, all_memory_flags));
+            commands.push_str(" ");
+            commands.push_str(&get_disk_stats(all_disk_flags));
         } else {
             if let Some(cpu_flags) = &cli.cpu {
-                commands.push(get_cpu_stats(&s, cpu_flags));
+                commands.push_str(&get_cpu_stats(
+                    &s,
+                    &cpu_flags.iter().map(String::as_str).collect::<Vec<&str>>(),
+                ));
+                commands.push_str(" ");
             }
 
             if let Some(memory_flags) = &cli.memory {
-                commands.push(get_memory_stats(&s, memory_flags));
+                commands.push_str(&get_memory_stats(
+                    &s,
+                    &memory_flags
+                        .iter()
+                        .map(String::as_str)
+                        .collect::<Vec<&str>>(),
+                ));
+                commands.push_str(" ");
             }
 
             if let Some(disk_flags) = &cli.disk {
-                commands.push(get_disk_stats(disk_flags));
+                commands.push_str(&get_disk_stats(
+                    &disk_flags.iter().map(String::as_str).collect::<Vec<&str>>(),
+                ));
+                commands.push_str(" ");
             }
         }
 
-        for cmd in &commands {
-            builder = builder.add_part(cmd.to_string());
-        }
-        let message = builder.build();
+        let message = commands.trim_end().to_string();
 
         if cli.verbose {
             println!("Current message: {}", message);
         }
-        task::spawn(async move { send_to_sketchybar("system_stats", message, cli.verbose).await });
+        send_to_sketchybar("system_stats", message, cli.verbose);
 
         thread::sleep(Duration::from_secs(cli.interval.into()));
     }
